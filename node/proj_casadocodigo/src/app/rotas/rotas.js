d5195b764631588a50635dc9ea46a0c85ac9485b
jotasimione <j.simione@users.noreply.github.com>
2019-04-22 13:18:39 -0300

    iniciando projeto node with marko

---
+++ node/proj_casadocodigo/src/app/rotas/rotas.js
@@ -0,0 +1,26 @@
+const db = require('../../config/database');
+
+module.exports = (app) => {
+    app.get('/livros',(req, resp) => {
+        db.all('SELECT * FROM livros',(erro, resultados) => {
+            resp.marko(require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
+    });
+
+    app.get('/',(req, resp) => {
+        resp.send(`
+            <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Casa do Código</h1>
+            </body>
+            </html>
+        `);
+    });
+}
