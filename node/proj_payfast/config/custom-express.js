cd056f7652fc1f0c47e96dc884327c6f497a072d
jotasimione <j.simione@users.noreply.github.com>
2019-05-07 13:17:58 -0300

    API

--- node/proj_payfast/config/custom-express.js
+++ node/proj_payfast/config/custom-express.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const expressValidator = require('express-validator');
 const consign = require('consign'); 
+const bodyParser = require('body-parser');
 const app =  express();
+
+app.use(expressValidator());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
     .include('controllers')
--- node/proj_payfast/controllers/pagametos-controller.js
+++ node/proj_payfast/controllers/pagametos-controller.js
@@ -1,5 +1,10 @@
 module.exports = (app) => {
     app.get('/pagamentos', (req, res) => {
         res.send('deu certo!');
-    })
+    });
+
+    app.post('/pagamentos/pagamento', (req, res) => {
+        console.log(req.body);
+        res.send('Post executado!');
+    });
 }
