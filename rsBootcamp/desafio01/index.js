66253eac335d2fc360b19143b380bd8b88e82c47
jotasimione <j.simione@users.noreply.github.com>
2019-08-21 21:22:38 -0300

    desadio concluido

--- rsBootcamp/desafio01/index.js
+++ rsBootcamp/desafio01/index.js
@@ -5,19 +5,27 @@ server.use(express.json());
 
 //{ id: "1", title: 'Novo projeto', tasks: [] }
 const projects = [];
+let reqCount = 0;
 
-function  checkIdProject(req,res,next){
-  if (!req.bode.id) {
-    res.state(400,'')
+function checkIdProject(req, res, next) {
+  if (!req.params.id) {
+    return res.status(400).json({ error: 'Project ID is required!' });
   }
+  return next();
 }
 
-server.post('/projects', (req, res) => {
+function countRequest(req, res, next) {
+  reqCount++;
+  console.log(`Request Count: ${reqCount}`);
+  return next();
+}
+
+server.post('/projects', countRequest, (req, res) => {
   projects.push(req.body);
-  return res.send('Projeto criado com sucesso!');
+  return res.send('Project was created with success!');
 });
 
-server.post('/projects/:id/tasks', (req, res) => {
+server.post('/projects/:id/tasks', countRequest, checkIdProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -30,18 +38,30 @@ server.post('/projects/:id/tasks', (req, res) => {
   return res.json(projects);
 });
 
-server.get('/projects', (req, res) => {
+server.get('/projects', countRequest, (req, res) => {
   return res.json(projects);
 });
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', countRequest, checkIdProject, (req, res) => {
   const { id } = req.params;
 
-  projects[].forEach(project => {
+  projects.forEach((project, index) => {
     if (project.id == id) {
-      projects[project.indexOf] = req.body;
+      projects[index] = req.body;
     }
   });
 
   return res.json(projects);
+});
+
+server.delete('/projects/:id', countRequest, checkIdProject, (req, res) => {
+  const { id } = req.params;
+
+  projects.forEach((project, index) => {
+    if (project.id == id) {
+      projects[index].delete;
+    }
+  });
+
+  return res.json('Project was deleted with success!');
 });
